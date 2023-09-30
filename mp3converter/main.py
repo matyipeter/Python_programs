d752881f354d0a449baa4922635a0bd4436ea0b0
peter <petermatyi2004@gmail.com>
2023-09-30 16:39:09 +0200

    Basic GUI configuration

--- mp3converter/main.py
+++ mp3converter/main.py
@@ -1,18 +1,40 @@
 from pytube import YouTube
+import tkinter as tk
+from tkinter import messagebox
 import os
 
 DESTINATION = "/home/peter/Music/"
 
-def convert():
-    yt = YouTube(
-        str(input("Enter the URL of the video you want to download: \n>> ")))
+class GUI():
+    def __init__(self):
+        self.root = tk.Tk()
 
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path=DESTINATION)
+        self.label = tk.Label(self.root, text='MP3 converter', font=('Arial', 18))
+        self.label.pack(padx=10, pady=10)
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt.title + " has been successfully downloaded.")
+        self.textbox = tk.Text(self.root, font=('Arial', 16))
+        self.textbox.pack(padx=10, pady=10)
+
+        self.button = tk.Button(self.root, text='Convert', font=('Arial', 18), command=self.convert)
+        self.button.pack(padx=15, pady=10)
+
+        self.root.mainloop()
+
+    def convert(self):
+        yt = YouTube(str(self.textbox.get('1.0', tk.END)))
+
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=DESTINATION)
+
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        messagebox.showinfo(title='Success!',message = yt.title + " has been successfully downloaded.")
   
-convert()    
+GUI()
+
+
+
+    
+
+
